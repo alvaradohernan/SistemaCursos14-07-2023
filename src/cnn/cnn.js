94d251bb94358f530c79f03a63d4fca1a72adf58
Hernán <hernnalvarado@gmail.com>
2023-07-17 22:52:47 -0400

    trabajando con railway

--- src/cnn/cnn.js
+++ src/cnn/cnn.js
@@ -1,3 +1,11 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize('postgres://postgres:qbvieSQcYMtxpvL569fr@containers-us-west-136.railway.app:7729/railway');
+export const sequelize = new Sequelize('postgres://postgres:ot6j1RE8PkyWIzNDmvpx@containers-us-west-149.railway.app:7872/railway');
+
+
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
